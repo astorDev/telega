9e528a74d8416da6080fc212dc3412dd2b4f0abf
Egor Tarasov <pavluchenk0@yandex.ru>
2024-11-26 16:04:05 +0400

    template init

---
+++ template/tests/AboutShould.cs
@@ -0,0 +1,16 @@
+namespace Telega.Tests;
+
+[TestClass]
+public class AboutShould : Test
+{
+    [TestMethod]
+    public async Task ReturnValidMetadata()
+    {
+        var about = await this.Client.GetAbout();
+        about.Should().BeEquivalentTo(new About(
+            "Telega",
+            "1.0.0.0",
+            "Development"
+        ));
+    }
+}
